8fb40dfbc458e348a78ee1565f80837cfb4b1bb6
zachary ladouceur-simard <118993100+zach-simard@users.noreply.github.com>
2022-12-06 10:03:32 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+})
+let counter = 0
+counter = 0
